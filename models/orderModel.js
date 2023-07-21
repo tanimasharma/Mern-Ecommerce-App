ad6579d5c0c0a5adb7fea4a822273ba82a513f4a
Tanima Sharma <tanimasharma2001@gmail.com>
2023-07-21 21:59:17 +0530

    final code
    ecommerce mern stack app

---
+++ models/orderModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payemnt: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "delivered", "Cancel"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Orders", orderSchema);
